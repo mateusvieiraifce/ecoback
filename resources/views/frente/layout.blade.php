4d4684f3c2ccd0e75b77e76ca4b930b961c8d115
mentrixmax <Jesus0804#>
2022-10-15 16:29:44 -0300

    corrigindo menu celular

--- resources/views/frente/layout.blade.php
+++ resources/views/frente/layout.blade.php
@@ -152,11 +152,11 @@
 					<i class="zmdi zmdi-search"></i>
 				</div>
 
-				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" data-notify="2">
+				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" data-notify="{{$nocarrinho}}">
 					<i class="zmdi zmdi-shopping-cart"></i>
 				</div>
 
-				<a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti" data-notify="0">
+				<a href="{{route('advertisement.listfavorito')}}"  class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti" data-notify="{{$favoritos}}">
 					<i class="zmdi zmdi-favorite-outline"></i>
 				</a>
 			</div>
