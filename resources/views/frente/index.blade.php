fb498455f3f8b57c6b527609b682e62ed48f8057
mentrixmax <Jesus0804#>
2022-10-15 16:20:28 -0300

    ajustes de formato de data

--- resources/views/frente/index.blade.php
+++ resources/views/frente/index.blade.php
@@ -388,7 +388,7 @@
                                                 </h4>
 
                                                 <span class="mtext-106 cl2">
-								R$  {{$anuncio->preco}}
+								@money($anuncio->preco)
 							</span>
 
                                                 <p class="stext-102 cl3 p-t-23">
