f127e81a0394627493755a3e4f4f0f30ac5632f7
mentrixmax <Jesus0804#>
2022-11-22 16:10:19 -0300

    corrigindo limpar pela descricao

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Anuncio;
 use App\Models\TagsAnuncio;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SiteController extends Controller
 {
